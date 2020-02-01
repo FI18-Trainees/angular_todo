010fe7ce5118fc72f6a6b691e01d57790f0a01ed
monkmitrad <monkmitrad@gmail.com>
2020-02-01 22:28:02 +0100

    added sidebar, todo details displayed in sidebar

--- angular/src/app/list-view/list-view.component.ts
+++ angular/src/app/list-view/list-view.component.ts
@@ -3,6 +3,7 @@ import { Observable, Subscription, Observer } from 'rxjs';
 import { Todo } from 'src/interfaces/todo';
 import { TodoService } from 'src/services/todo.service';
 import { MatCheckboxChange } from '@angular/material';
+import { SidenavService } from 'src/services/sidenav.service';
 
 @Component({
   selector: 'app-list-view',
@@ -15,7 +16,7 @@ export class ListViewComponent implements OnInit, OnDestroy {
   finishedTodos: Todo[] = [];
   private todoSubscription: Subscription;
 
-  constructor(private todoService: TodoService) {
+  constructor(private todoService: TodoService, private sidenavService: SidenavService) {
     this.todoSubscription = this.todoService.todoSub().subscribe((todo: Todo) => {
         if (todo.finished) {
           const index: number = this.openTodos.indexOf(todo);
@@ -41,7 +42,15 @@ export class ListViewComponent implements OnInit, OnDestroy {
     this.todoSubscription.unsubscribe();
   }
 
-  todoFinished(evt: MatCheckboxChange) {
-    this.todoService.finishTodo(+evt.source.id);
+  todoFinished(id: number) {
+    this.todoService.finishTodo(id);
+  }
+
+  showOpenTodoDetails(id: number) {
+    this.sidenavService.open(this.openTodos[id]);
+  }
+
+  showFinishedTodoDetails(id: number) {
+    this.sidenavService.open(this.finishedTodos[id]);
   }
 }
